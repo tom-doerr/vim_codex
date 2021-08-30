3f55375d62d347a0653a9309f5fb515323cd2be3
Tom Dörr <tom.doerr@tum.de>
2021-08-30 19:20:05 +0200

    Add stream support

--- python/plugin.py
+++ python/plugin.py
@@ -14,11 +14,12 @@ from AUTH import *
 openai.organization = ORGANIZATION_ID
 openai.api_key = SECRET_KEY
 MAX_SUPPORTED_INPUT_LENGTH = 4096
+USE_STREAM_FEATURE = True
 
 def complete_input_max_length(input_prompt, max_input_length=MAX_SUPPORTED_INPUT_LENGTH):
     input_prompt = input_prompt[-max_input_length:]
 
-    response = openai.Completion.create(engine='davinci-codex', prompt=input_prompt, best_of=1, temperature=0.1, max_tokens=64)
+    response = openai.Completion.create(engine='davinci-codex', prompt=input_prompt, best_of=1, temperature=0.5, max_tokens=64, stream=USE_STREAM_FEATURE)
     return response
 
 def complete_input(input_prompt):
@@ -33,7 +34,6 @@ def complete_input(input_prompt):
 
 def create_completion(): 
     vim_buf = vim.current.buffer
-    vim_win = vim.current.window
     input_prompt = '\n'.join(vim_buf[:])
     
     row, col = vim.current.window.cursor
@@ -41,22 +41,44 @@ def create_completion():
     input_prompt += '\n'.join(vim_buf[:row-2])
     input_prompt += '\n' + vim_buf[row-1][:col]
     response = complete_input(input_prompt)
-    completion = response['choices'][0]['text']
-    current_line = vim.current.buffer[row-1]
-    new_line = current_line[:col] + completion + current_line[col:]
-    if new_line[-1] == '\n':
-        new_line = new_line[:-1]
-    new_lines = new_line.split('\n')
-    new_lines.reverse()
-    vim_buf[row-1] = None
-    cursor_pos_base = tuple(vim_win.cursor)
-    for row_i in range(len(new_lines)):
-        vim.current.buffer[row-1:row-1] = [new_lines[row_i]]
+    write_response(response)
 
-    if new_line[-1] != '\n':
-        cursor_target_col = len(new_lines[0])
-    else:
-        cursor_target_col = 0
-    vim_win.cursor = (cursor_pos_base[0] + row_i + 1, cursor_target_col)
+def write_response(response):
+    vim_buf = vim.current.buffer
+    vim_win = vim.current.window
+    while True:
+        if USE_STREAM_FEATURE:
+            single_response = next(response)
+        else:
+            single_response = response
+        if single_response['choices'][0]['finish_reason'] != None: break
+        completion = single_response['choices'][0]['text']
+        row, col = vim.current.window.cursor
+        current_line = vim.current.buffer[row-1]
+        new_line = current_line[:col] + completion + current_line[col:]
+        if not USE_STREAM_FEATURE:
+            if new_line == '':
+                new_line = new_line
+            elif new_line[-1] == '\n':
+                new_line = new_line[:-1]
+        new_lines = new_line.split('\n')
+        new_lines.reverse()
+        vim_buf[row-1] = None
+        cursor_pos_base = tuple(vim_win.cursor)
+        for row_i in range(len(new_lines)):
+            vim.current.buffer[row-1:row-1] = [new_lines[row_i]]
 
+        if new_line == '':
+            cursor_target_col = 0
+        elif new_line[-1] != '\n':
+            cursor_target_col = len(new_lines[0])
+        else:
+            cursor_target_col = 0
+        vim_win.cursor = (cursor_pos_base[0] + row_i + 1, cursor_target_col)
+
+        if not USE_STREAM_FEATURE:
+            break
+
+        # Flush the vim buffer.
+        vim.command("redraw")
 
